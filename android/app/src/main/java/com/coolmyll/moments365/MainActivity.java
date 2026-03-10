1f5e679cdc2c7e4d3ffffdb7a1303490bd33a1cb
Cool <cool_myll@hotmail.com>
2026-03-10 17:38:27 +0200

    Menu fixes

--- android/app/src/main/java/com/coolmyll/moments365/MainActivity.java
+++ android/app/src/main/java/com/coolmyll/moments365/MainActivity.java
@@ -2,8 +2,10 @@ package com.coolmyll.moments365;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
@@ -16,8 +18,19 @@ public class MainActivity extends BridgeActivity {
     @Override
     public void onStart() {
         super.onStart();
-        WindowCompat.setDecorFitsSystemWindows(getWindow(), true);
+        // Let the app draw behind system bars
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
         Window window = getWindow();
         window.setStatusBarColor(Color.parseColor("#16213e"));
+        
+        // Add padding to the content view to prevent content from appearing behind status bar
+        View contentView = findViewById(android.R.id.content);
+        if (contentView != null) {
+            contentView.setOnApplyWindowInsetsListener((v, insets) -> {
+                int systemBarsTop = insets.getInsets(WindowInsetsCompat.Type.systemBars()).top;
+                v.setPadding(0, systemBarsTop, 0, 0);
+                return insets;
+            });
+        }
     }
 }
